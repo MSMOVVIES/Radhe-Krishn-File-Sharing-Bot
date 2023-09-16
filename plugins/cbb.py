53c4d2a2cb9c1fbba4d09bccd579cf1e09c81a51
MSMOVVIES <142180881+MSMOVVIES@users.noreply.github.com>
2023-09-16 16:36:18 +0530

    cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>‣ ᴍʏ ɴᴀᴍᴇ : ᴘᴇʀᴍᴀɴᴇɴᴛ\n‣ ᴅᴀᴛᴀʙᴀꜱᴇ : ᴄʟᴏᴜᴅ\n‣ ꜱᴇʀᴠᴇʀ : ʙᴏꜱꜱ ʜᴏᴍᴇ\n‣ ᴄʀᴇᴀᴛᴏʀ: <a href='https://t.me/MS_Contact_RoBot'>ᴍs_ʜᴀᴄᴋᴇʀ</a>\n‣ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/MS_Movvies'>ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ</a>\n‣ вᴜɪʟᴅ ѕᴛᴀᴛᴜѕ: v2.7.1 [ ѕᴛᴀʙʟᴇ ]</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🔙 ʙᴀᴄᴋ 🔙", callback_data = "start")
                     ]
                 ]
             )
